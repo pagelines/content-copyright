ea575cf22176721162811835afc46d84efb7b8db
William Mincy <william.mincy@jwt.com>
2013-08-17 11:25:27 -0400

    + Added support for displaying the copyright year in Roman numerals

--- section.php
+++ section.php
@@ -47,20 +47,27 @@ class wmContentCopyright extends PageLinesSection {
 							),
 						array (
 								'key' => 'WMCopyright_AllRightsReserved', 
-								'type' => 'check', 
+								'type' => 'check',
 								'title' => __('Show "All Rights Reserved" statement','wmContentCopyright'),
 								'label' => __('Show "All Rights Reserved" statement','wmContentCopyright'),
 								'default' => true
 							),
 						array(
+								'key' => 'WMCopyright_Roman',
+								'type' => 'check',
+								'title' => __('Show as Roman numerals','wmContentCopyright'),
+								'label' => __('Show as Roman numerals','wmContentCopyright'),
+								'default' => false
+							),
+						array(
 								'key' => 'WMCopyright_BeginningYear',
-								'type' => 'text', 
+								'type' => 'text',
 								'title' => __('Copyright beginning year','wmContentCopyright'),
 								'label' => __('Copyright beginning year','wmContentCopyright')
 							),
 						array(
 								'key' => 'WMCopyright_MultipleYears',
-								'type' => 'check', 
+								'type' => 'check',
 								'title' => __('Show multiple years','wmContentCopyright'),
 								'label' => __('Show multiple years','wmContentCopyright'),
 								'ref' => __('Checking this will show the year over time to be the start year up through the currnet year (i.e. 2010 - 2013) as opposed to the currene year (2013)','wmContentCopyright'),
@@ -71,6 +78,70 @@ class wmContentCopyright extends PageLinesSection {
 		return $options;
 	}
 	
+	function toRoman($num, $uppercase = true)
+    {
+        $conv = array(10 => array('X', 'C', 'M'), 
+        			5 => array('V', 'L', 'D'),
+        			1 => array('I', 'X', 'C')
+        		);
+        $roman = '';
+
+        if ($num < 0) {
+            return '';
+        }
+
+        $num = (int) $num;
+        $digit = (int) ($num / 1000);
+        $num -= $digit * 1000;
+
+        while ($digit > 0) {
+            $roman .= 'M';
+            $digit--;
+        }
+
+        for ($i = 2; $i >= 0; $i--) {
+            $power = pow(10, $i);
+            $digit = (int) ($num / $power);
+            $num -= $digit * $power;
+
+            if (($digit == 9) || ($digit == 4)) {
+                $roman .= $conv[1][$i] . $conv[$digit+1][$i];
+            } else {
+                if ($digit >= 5) {
+                    $roman .= $conv[5][$i];
+                    $digit -= 5;
+                }
+
+                while ($digit > 0) {
+                    $roman .= $conv[1][$i];
+                    $digit--;
+                }
+            }
+        }
+
+        $roman = str_replace(str_repeat('M', 1000), 'AFS', $roman);
+        $roman = str_replace(str_repeat('M', 900), 'CAFS', $roman);
+        $roman = str_replace(str_repeat('M', 500), 'D', $roman);
+        $roman = str_replace(str_repeat('M', 400), 'CD', $roman);
+        $roman = str_replace(str_repeat('M', 100), 'C', $roman);
+        $roman = str_replace(str_repeat('M', 90), 'XC', $roman);
+        $roman = str_replace(str_repeat('M', 50), 'L', $roman);
+        $roman = str_replace(str_repeat('M', 40), 'XL', $roman);
+        $roman = str_replace(str_repeat('M', 10), 'X', $roman);
+        $roman = str_replace(str_repeat('M', 5), 'V', $roman);
+        $roman = str_replace(str_repeat('M', 4), 'MV', $roman);
+
+        $roman = str_replace('AFS', 'M', $roman);
+
+        if ($uppercase == false) {
+            $roman = strtolower($roman);
+        }
+
+       return $roman;
+
+    }
+
+	
 	// Content Display
 	function section_template() {
 		echo '<span class="wmContentCopyright">';
@@ -80,9 +151,9 @@ class wmContentCopyright extends PageLinesSection {
 		}
 		$startyear = (ploption('WMCopyright_BeginningYear',$this->oset)) ? ploption('WMCopyright_BeginningYear',$this->oset) : (int)date("Y");
 		$thisyear = date("Y");
-		echo ' '.$startyear.' ';
+		echo ' '.((ploption('WMCopyright_Roman', $this->oset)) ? $this->toRoman($startyear) : $startyear).' ';
 		if(ploption('WMCopyright_MultipleYears',$this->oset) && $startyear<$thisyear) {
-			echo '- '.$thisyear.' ';
+			echo '- '.((ploption('WMCopyright_Roman', $this->oset)) ? $this->toRoman($thisyear) : $thisyear).' ';
 		}
 		if(ploption('WMCopyright_Title', $this->oset)) { 
 			echo ploption('WMCopyright_Title', $this->oset);
